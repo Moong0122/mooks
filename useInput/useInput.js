b8a2e0481a1471496f333da6d290e7df0f945136
Moong0122 <2015004802@hanyang.ac.kr>
2020-03-30 16:48:11 +0900

    react hooks(useState) studying...

---
+++ useInput/useInput.js
@@ -0,0 +1,14 @@
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) setValue(value);
+  };
+  return { value, onChange };
+};
